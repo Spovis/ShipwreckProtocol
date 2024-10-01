42584eaf76a3a710e04b15cf28a3a2456da25a91
Alice <157913552+robberfish@users.noreply.github.com>
2024-09-30 20:15:29 -0700

    adding enemy

---
+++ Assets/enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemy : MonoBehaviour
+{
+    public Transform player;
+    public float detects_range = 5f;
+
+    //update is called once per frame
+    void Update()
+    {
+        detecting_player();
+    }
+
+    //detects the player if they are in range
+    private void detecting_player()
+    {
+        float dist_to_player = Vector3.Distance(transform.position, player.position);
+
+        if (dist_to_player <= detects_range){
+            Debug.Log("Player is detected within range! The distance= " + dist_to_player);
+            // Call specific function when the player is in range(some kind of attack function for example)
+        }else{
+            Debug.Log("Player is not in range. The distance= " + dist_to_player);
+        }
+    }
+}
+
+
