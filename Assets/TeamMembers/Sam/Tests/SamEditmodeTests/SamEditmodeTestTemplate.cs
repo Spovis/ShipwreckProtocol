d5874e81d6c7b35e37ce8bd561da69b4be8274de
robi7030 <159516482+robi7030@users.noreply.github.com>
2024-10-16 23:02:52 -0700

    Tests

--- Assets/TeamMembers/Sam/Tests/SamEditmodeTests/SamEditmodeTestTemplate.cs
+++ Assets/TeamMembers/Sam/Tests/SamEditmodeTests/SamEditmodeTestTemplate.cs
@@ -5,31 +5,21 @@ using UnityEngine.TestTools;
 
 public class SamEditmodeTestTemplate {
 
-    /* 
-     * This is basically an empty test script. This test simply creates an empty GameObject and asserts that it is active.
-     * The purpose of this existing is to ensure that tests are working for everyone. You can edit this test to be one you need,
-     * or delete it and create your own test script. It also works as a basic template for a boundary test script.
-    */
-
-    GameObject testObj;
 
     [UnitySetUp]
     public IEnumerator Setup()  // Ensure this method is IEnumerator for coroutines
     {
-        // Creates a new GameObject and assigns it to the testObj variable
-        testObj = new GameObject("TestObject");
-
-        // Yield to allow for initialization
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/TeamMembers/Sam/Tests/SamEditmodeTests/TestSampleScene.unity");
         yield return null;
     }
 
 
 
     [UnityTest]
-    public IEnumerator TestObjectCreation() {
+    public IEnumerator TestRunRoom() {
 
-        // Assert that the test object is now active
-        Assert.IsTrue(testObj.activeSelf, "Test object should be active when the test runs.");
+        global::System.Object value = UnityEditor.SceneManagement.EditorSceneManager.LoadSceneInPlayMode(TestSampleScene);
+
 
         yield return null;
     }
